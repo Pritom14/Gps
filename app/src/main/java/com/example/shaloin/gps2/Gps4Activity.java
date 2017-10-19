337a3fd96d57094174c2d995140a701223cffb8b
Pritom14 <pritommazumdar1995@gmail.com>
2017-10-19 21:25:04 +0530

    UI Tweaks

--- app/src/main/java/com/example/shaloin/gps2/Gps4Activity.java
+++ app/src/main/java/com/example/shaloin/gps2/Gps4Activity.java
@@ -42,26 +42,23 @@ public class Gps4Activity extends AppCompatActivity implements
     private GoogleApiClient mGoogleApiClient;
     private static final int PERMISSION_REQUEST_CODE = 100;
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
-    private TextView display1,display2,display3,display4,display5;
-    private Button button;
-    String number="";
+    private TextView display;
+    private Button location_button,contacts_button;
+    String number="+919707153020";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps4);
-        button=(Button)findViewById(R.id.showL);
-        display1=(TextView)findViewById(R.id.location2);
-        display2=(TextView)findViewById(R.id.location3);
-        display3=(TextView)findViewById(R.id.location4);
-        display4=(TextView)findViewById(R.id.location5);
-        display5=(TextView)findViewById(R.id.location6);
+        location_button=(Button)findViewById(R.id.show_button);
+        contacts_button=(Button)findViewById(R.id.view_button);
+        display=(TextView)findViewById(R.id.location_textview);
 
         mGoogleApiClient = new GoogleApiClient.Builder(Gps4Activity.this)
                 .addApi(Places.PLACE_DETECTION_API)
                 .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
                 .build();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        location_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mGoogleApiClient.isConnected()) {
@@ -74,34 +71,17 @@ public class Gps4Activity extends AppCompatActivity implements
                         ActivityCompat.requestPermissions(Gps4Activity.this,
                                 new String[]{Manifest.permission.SEND_SMS},
                                 MY_PERMISSIONS_REQUEST_SEND_SMS);
-                    } else {
-                        final AlertDialog.Builder dialogBuilder=new AlertDialog.Builder(Gps4Activity.this);
-                        LayoutInflater inflater=Gps4Activity.this.getLayoutInflater();
-                        final View dialogView=inflater.inflate(R.layout.custom_dialog,null);
-                        dialogBuilder.setView(dialogView);
-
-                        final EditText phoneN=(EditText)dialogView.findViewById(R.id.dialogEditPhID);
-                        dialogBuilder.setTitle("Send Location");
-                        dialogBuilder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                String number2=phoneN.getText().toString();
-                                number=number2;
-                                callPlaceDetectionApi();
-                            }
-                        });
-
-                        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                        AlertDialog b=dialogBuilder.create();
-                        b.show();
                     }
                 }
+                callPlaceDetectionApi();
+            }
+        });
+
+        contacts_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Gps4Activity.this,DetailsActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -135,24 +115,6 @@ public class Gps4Activity extends AppCompatActivity implements
         }
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.add_number:
-
-
-
-            default:
-            return super.onOptionsItemSelected(item);
-        }
-    }*/
 
     private void callPlaceDetectionApi() throws SecurityException {
         PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
@@ -165,27 +127,9 @@ public class Gps4Activity extends AppCompatActivity implements
                                     "likelihood: %g",
                             placeLikelihood.getPlace().getName(),
                             placeLikelihood.getLikelihood()));
-//                    Toast.makeText(getApplicationContext(),String.format("Place '%s' with " +
-//                                    "likelihood: %g",
-//                            placeLikelihood.getPlace().getName(),
-//                            placeLikelihood.getLikelihood()),Toast.LENGTH_LONG).show();
-//                    display.setText(String.format("Place '%s' with " +
-//                                    "likelihood: %g",
-//                            placeLikelihood.getPlace().getName(),
-//                            placeLikelihood.getLikelihood()));
-                    //display1.setText(placeLikelihood.getPlace().getName().toString());
-                    display2.setText(placeLikelihood.getPlace().getAddress().toString());
-
-
-//                    sendMySMS(placeLikelihood.getPlace().getAddress().toString());
-//                    SmsManager smsManager = SmsManager.getDefault();
-//                    smsManager.sendTextMessage(number, null, placeLikelihood.getPlace().getAddress().toString(),
-//                            null, null);
-//                    Toast.makeText(getApplicationContext(), "SMS sent.",
-//                            Toast.LENGTH_LONG).show();
+                    display.setText(placeLikelihood.getPlace().getAddress().toString());
                     messageSending(placeLikelihood.getPlace().getAddress().toString());
                     break;
-                    //likelyPlaces.release();
                 }
                 likelyPlaces.release();
 
